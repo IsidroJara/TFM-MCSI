c0c5705373b6fc5e17df1fc31f039dac1eb20670
Isidro Jara Matas <isidro@MacBook-Pro-de-Isidro.local>
2019-11-02 14:58:17 +0100

    Update Scheduler

--- Scheduler/scheduleCron.py
+++ Scheduler/scheduleCron.py
@@ -1,7 +1,14 @@
+import os
 from crontab import CronTab
  
+dirpath = os.getcwd()
+
+papermill_path = "/Users/isidro/anaconda3/bin/papermill"
+input = dirpath + "/Untitled.ipynb"
+output = dirpath + "/output.ipynb"
+
 my_cron = CronTab(user='isidro')
-job = my_cron.new(command='jupyter nbconvert --to notebook --execute Untitled.ipynb')
+job = my_cron.new(command='papermill_path input output')
 job.minute.every(1)
  
 my_cron.write()
